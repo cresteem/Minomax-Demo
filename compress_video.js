ed93de6eba4c7faf55e62db31f20cc189644f606
PRIYADARSAN S <iamspdarsan@gmail.com>
2024-07-19 22:21:19 +0530

    video compression results & report

--- compress_video.js
+++ compress_video.js
@@ -3,13 +3,13 @@ const { compressVideos } = require("@cresteem/minomax");
 const pathPatterns = ["**/*.mp4", "**/*.mkv", "**/*.webm"];
 const codecType = "wav1"; // "wav1" | "mav1" | "mx265"
 const encodeLevel = 3; //1 | 2 | 3,
-const destinationBasePath = "./dest";
-const ignorePatterns = [];
+const destinationBasePath = "./video_results";
+const ignorePatterns = ["./dist/**"];
 
-await compressVideos(
+compressVideos(
   pathPatterns,
   codecType,
   encodeLevel,
   destinationBasePath,
   ignorePatterns
-);
+).catch(console.log);
